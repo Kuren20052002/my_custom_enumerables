a044dfda13e501d2d16d9892b986a7472b56a7ea
Nguyễn Hoàng Lâm <165152517+Kuren20052002@users.noreply.github.com>
2024-07-19 00:03:04 +0700

    complete my_each_with_index

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -30,6 +30,15 @@ module Enumerable
     end
     count
   end
+
+  def my_each_with_index
+    index = 0
+    while index < self.length
+      yield(self[index], index)
+      index += 1
+    end
+    self
+  end
 end
 
 # You will first have to define my_each
