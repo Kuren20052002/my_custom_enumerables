41670bec8a6b7f9420b80e66f6295e0aa5ffdb85
Nguyễn Hoàng Lâm <165152517+Kuren20052002@users.noreply.github.com>
2024-07-18 23:37:15 +0700

    Complete each, all, any and count

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,35 @@
 module Enumerable
-  # Your code goes here
+  def my_all?
+    self.my_each do |element|
+      return false if !yield(element)
+    end
+
+    true
+  end
+
+  def my_any?
+    self.my_each do |element|
+      return true if yield(element)
+    end
+
+    false
+  end
+
+  def my_count(number = 0)
+    count = 0
+    if block_given?
+      self.my_each do |element|
+        count += 1 if yield(element)
+      end
+    elsif number != 0
+      self.my_each do |element|
+        count += 1 if element == number
+      end
+    else
+      return self.length
+    end
+    count
+  end
 end
 
 # You will first have to define my_each
@@ -7,5 +37,10 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each
+    for element in self
+      yield(element)
+    end
+    self
+  end
 end
