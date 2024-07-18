4dbcc907b9dd7226f40c0f7f2eed6e9e112b194b
Nguyễn Hoàng Lâm <165152517+Kuren20052002@users.noreply.github.com>
2024-07-18 23:40:41 +0700

    Add to my_count to count occurrences of the arguement in the array and add tests

--- spec/my_count_spec.rb
+++ spec/my_count_spec.rb
@@ -4,6 +4,7 @@ require_relative '../lib/my_enumerables'
 
 RSpec.describe Enumerable do
   subject(:enumerable) { [1, 1, 2, 3, 5, 8, 13, 21, 34] }
+  subject(:lol) { [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5] }
 
   describe '#my_count' do
     context 'when not given a block' do
@@ -17,6 +18,16 @@ RSpec.describe Enumerable do
         expect(enumerable.my_count { |value| value > 5 }).to eq 4
         expect(enumerable.my_count { |value| value <= 5 }).to eq 5
         expect(enumerable.my_count { |value| value == 5 }).to eq 1
+      end
+    end
+
+    context 'when given an arguement' do
+      it 'returns the count of the elements that satisfy the arguement' do
+        expect(lol.my_count(1)).to eq 1
+        expect(lol.my_count(2)).to eq 2
+        expect(lol.my_count(3)).to eq 3
+        expect(lol.my_count(4)).to eq 4
+        expect(lol.my_count(5)).to eq 5
       end
     end
   end
